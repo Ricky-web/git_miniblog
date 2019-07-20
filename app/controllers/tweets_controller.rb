fd3a48184f3cccc3549be8d47fdc90716dfa26df
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-07-20 07:23:32 +0000

    Added flash message

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,8 +8,13 @@ class TweetsController < ApplicationController
   end
   
   def create
-    Tweet.create(text: tweet_params[:text], user_id: current_user.id)
-    redirect_to :root
+    if text_confirmation
+      redirect_to new_tweet_path, alert: '▼ Please enter the text.'
+      
+    else
+      Tweet.create(text: tweet_params[:text], user_id: current_user.id)
+      redirect_to :root
+    end
   end
   
   def show
@@ -21,8 +26,12 @@ class TweetsController < ApplicationController
   end
   
   def update
-    Tweet.update(tweet_params)
-    redirect_to :root
+    if text_confirmation
+      redirect_to "/tweets/#{params[:id]}/edit", alert: '▼ Please enter the text.'
+    else
+      Tweet.update(tweet_params)
+      redirect_to :root
+    end
   end
   
   def destroy
@@ -36,4 +45,8 @@ class TweetsController < ApplicationController
     params.permit(:text)
   end
   
+  def text_confirmation
+    text = params.permit(:text)
+    return text[:text].blank?
+  end
 end
